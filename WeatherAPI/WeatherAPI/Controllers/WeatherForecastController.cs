31cedb181f83682e5aa55c0b54569535ce2c0f98
nasir <nasir016@gmail.com>
2023-10-22 11:41:51 +0200

    Added profile arm

--- WeatherAPI/WeatherAPI/Controllers/WeatherForecastController.cs
+++ WeatherAPI/WeatherAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WeatherAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm2", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
